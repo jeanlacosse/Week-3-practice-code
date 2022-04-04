08ba43005a7dfc31c1cc0c89e0fd82a7dd5fc8c1
jeanlacosse <jeanlacosse@gmail.com>
2022-04-04 09:41:26 -0500

    Arrays and loops practice

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-console.log('hello!')
+console.log('This practice is for arrays!');
+
+let bagel = ['bacon', 'lettuce', 'tomato'];
+
+bagel[bagel.length - 1]
+// ^ will give me the last item in the arrays
+
+bagel[0] = 'maple bacon';
+// ^ change the item in the array by referencing index
+
+bagel.indexOf('cheese');
+// ^ will return -1
+
+bagel.push('mustard');
+// ^.push adds to the end of the array
+
+bagel.pop()
+// ^.pop removes last item, 'mustard'
+bagel.push('mayo');
+bagel.push('asiago');
+
+bagel.unshift('asiago');
+// ^ .unshift adds item to the beginning
+
+bagel.shift();
+bagel.unshift('sesame');
+
+
+console.log('Here is the loops section!')
+
+// while loops below
+let index = 0;
+const highest = 10;
+
+while (index < highest) {
+console.log('new index is:', index);
+index++;
+}
